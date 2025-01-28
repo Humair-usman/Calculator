704beea9d754b3e48614919f64675b16fe4617f2
Humair-usman <humairusman005@gmail.com>
2025-01-28 22:19:15 +0500

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,23 @@
+//  var inputBox = document.getElementById('inputBox')
+//  let  buttons = document.querySelectorAll('button')
+//  let string = ''
+//  buttons.forEach (button =>{
+// element.addEventListener('click',(e) =>{
+//         if(e.target.Text == '='){
+//             string =String( eval(string))
+//             inputBox.value = string;
+//         }
+        // else if (e.target.innerHTMl == 'AC'){
+        //     string = "";
+        //     input.value = string;
+
+        // }
+        // else if(e.target.innerHTMl == 'DEl') {
+        //     string = string.substring(0,string.length-1)
+        //     input.value = string 
+        // }
+//         else{
+//         string+= e.target.Text;
+//         input.Box = string;}
+//  })
+//  })
